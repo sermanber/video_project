a545eb793ac6bdb53f93448d24df28a8840f59bf
sermanber <154537447+sermanber@users.noreply.github.com>
2024-04-04 01:20:05 +0300

    Add files via upload

---
+++ video project.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+const preloder = document.querySelector('.preloader');
+
+
+
+btn.addEventListener('click', function(){
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide');
+        video.pause();
+    }
+    else {
+        btn.classList.remove('slide');
+        video.play();
+    }
+});
+
+window.addEventListener('load', function(){
+    preloder.classList.add('hide-preloader');
+}); 
